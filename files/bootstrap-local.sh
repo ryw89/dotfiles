6dfc45d05719226a382eeeee3506f2d59737cc74
Ryan Whittingham <ryanwhittingham89@gmail.com>
2023-12-01 19:26:17 -0500

    Who needs dry?

---
+++ files/bootstrap-local.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Run as root.
+
+PKG_LIST=".pkgs.tmp"
+REPO="https://ryanwhittingham.com/bootstrap.git"
+REPO_NAME="bootstrap"
+
+grep '-' pkg.yml | cut -d '-' -f2 | sed 's/^[[:space:]]//g' > $PKG_LIST
+
+pacman -S --needed - < $PKG_LIST
+
+rm $PKG_LIST
+
+ansible-playbook site.yml --ask-become-pass
