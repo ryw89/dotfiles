863bc13c0f63ff37c89012192c6cbe2d40a61e40
Ryan Whittingham <ryanwhittingham89@gmail.com>
2023-12-01 19:26:31 -0500

    Bootstrap web changes

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -1,13 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Run as root.
 
 PKG_LIST=".pkgs.tmp"
+REPO="https://ryanwhittingham.com/bootstrap.git"
+REPO_NAME="bootstrap"
 
 grep '-' pkg.yml | cut -d '-' -f2 | sed 's/^[[:space:]]//g' > $PKG_LIST
 
 pacman -S --needed - < $PKG_LIST
 
 rm $PKG_LIST
+
+git clone $REPO $REPO_NAME
+cd $REPO_NAME
 
 ansible-playbook site.yml --ask-become-pass
